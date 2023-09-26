e675724d0ad7ea4b1fe69584f1be988a2f1536d9
Carlos Aznarán <caznaranl@uni.pe>
2023-09-26 00:16:18 -0500

    Advance

---
+++ src/Python/newton.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+
+from scipy.integrate import newton_cotes
+import numpy as np
+
+
+def f(x):
+    return np.sin(x)
+
+
+a = 0
+b = np.pi
+exact = 2
+for N in [2, 4, 6, 8, 10]:
+    x = np.linspace(a, b, N + 1)
+    an, B = newton_cotes(N, 1)
+    dx = (b - a) / N
+    quad = dx * np.sum(an * f(x))
+    error = abs(quad - exact)
+    print("{:2d}  {:10.9f}  {:.5e}".format(N, quad, error))
